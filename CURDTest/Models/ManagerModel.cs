724c37818be1b2278a4f3a5a3ff219ca924bdae9
ChanLance <bill77417@gmail.com>
2019-04-25 17:35:47 +0800

    專案更新

--- CURDTest/Models/ManagerModel.cs
+++ CURDTest/Models/ManagerModel.cs
@@ -11,5 +11,29 @@ namespace DataModels
         /// </summary>
         [Display(Name = "名稱")]        
         public string Name { get; set; }
+
+        /// <summary>
+        /// 客戶ID
+        /// </summary>
+        [Display(Name = "客戶ID")]
+        public string UserID { get; set; }
+
+        /// <summary>
+        /// 年齡
+        /// </summary>
+        [Display(Name = "年齡")]
+        public int Age { get; set; }
+
+        /// <summary>
+        /// 客戶住址
+        /// </summary>
+        [Display(Name = "客戶住址")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        [Display(Name = "建立時間")]
+        public DateTime CreateTime { get; set; }
     }
 }
